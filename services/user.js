9781f4eb4dfbf3e8dc492e9db3b3ae3770a45d25
nohearth <nohearth@users.noreply.github.com>
2020-08-03 10:04:14 -0400

    some validations

--- services/user.js
+++ services/user.js
@@ -1,4 +1,5 @@
 const User = require('../models').User
+const { throwException } = require('../utils/validation')
 
 
 async function createUser(data) {
@@ -10,6 +11,9 @@ async function getUser(id) {
   const user =  await User.findOne({
     where: {id}
   })
+  if(!user) {
+    throwException(`The User with the ID ${id} don't exits`)
+  }
   return user
 }
 
